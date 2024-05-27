43cbe738787efdd35a317390c7b2d6902f2cf063
masato-210831 <b86500b@gmail.com>
2024-05-27 14:45:06 +0900

    Webアプリケーションんを実装（Render前）

--- src/chatbot_engine.py
+++ src/chatbot_engine.py
@@ -1,3 +1,18 @@
-def chat(message:str) -> str:
-  return f"Hello! you said: {message}"
+from langchain.chat_models import ChatOpenAI
+import langchain
+from langchain.memory import ChatMessageHistory
+from langchain.schema import HumanMessage
+
+langchain.verbose = True
+
+
+ 
+
+def chat(message:str, history: ChatMessageHistory) -> str:
+  llm = ChatOpenAI(temperature=0)
+  
+  messages = history.messages
+  messages.append(HumanMessage(content=message))
+  
+  return llm(messages).content
 
--- src/gradio.app.py
+++ src/gradio.app.py
@@ -1,19 +1,28 @@
-import random
-import time
 import gradio as gr
 from dotenv import load_dotenv
 from chatbot_engine import chat
+from langchain.memory import ChatMessageHistory
+import os
+
+ 
+def respond(message, chat_history):
+    history = ChatMessageHistory()
+    
+    # gradioのchatbotは履歴を返し、[user_Message, ai_message]の配列 
+    # -> ChatMessageHistoryの様式に従ってuser, aiの履歴に格納
+    for [user_message, ai_message] in chat_history:
+        history.add_user_message(user_message)
+        history.add_ai_message(ai_message)
+
+    bot_message = chat(message, history)
+    chat_history.append((message, bot_message))
+    return "", chat_history
+ 
  
 with gr.Blocks() as demo:
     chatbot = gr.Chatbot()
     msg = gr.Textbox()
     clear = gr.Button("Clear")
- 
-    def respond(message, chat_history):
-        bot_message = chat(message)
-        chat_history.append((message, bot_message))
-        time.sleep(1)
-        return "", chat_history
  
     msg.submit(respond, [msg, chatbot], [msg, chatbot])
     clear.click(lambda: None, None, chatbot, queue=False)
@@ -21,4 +30,15 @@ with gr.Blocks() as demo:
   
 if __name__ == "__main__":
   load_dotenv()
-  demo.launch()
+  
+  # APP_ENVが見つからなかったら、productionを使用
+  app_env = os.environ.get("APP_ENV", "production")
+  
+  if app_env == "production":
+      username = os.environ["GRADIO_USERNAME"]
+      password = os.environ["GRADIO_PASSWORD"]
+      auth = (username, password)
+  else:
+      auth = None
+  
+  demo.launch(auth=auth)
