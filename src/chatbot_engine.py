b3c33d2786944d95d0bb8139972ab07fe361a5fd
masato-210831 <b86500b@gmail.com>
2024-05-28 17:24:16 +0900

    slackbotを実装

--- src/chatbot_engine.py
+++ src/chatbot_engine.py
@@ -1,21 +1,53 @@
-from langchain.chat_models import ChatOpenAI
 import langchain
-from langchain.memory import ChatMessageHistory
-from langchain.schema import HumanMessage
+from langchain.agents import AgentType, initialize_agent
+from langchain.agents.agent_toolkits import VectorStoreInfo, VectorStoreToolkit
+from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import DirectoryLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain.memory import ChatMessageHistory, ConversationBufferMemory
+from langchain.tools import BaseTool
+from typing import List
+
+# ベクトル化
+def create_index() -> VectorStoreIndexWrapper:
+    loader = DirectoryLoader("./src/", glob="**/*.py")
+    return VectorstoreIndexCreator().from_loaders([loader])
+
+# toolsはagentが使えるアクションみたいなもの
+# indexのvectorstoreをagentが使えるようにtoolkitに変換
+def create_tools(index: VectorStoreIndexWrapper, llm) -> List[BaseTool]:
+    vectorstore_info = VectorStoreInfo(
+        vectorstore=index.vectorstore,
+        name="udemy-langchain source code",
+        description="Source code of application named udemy-langchain",
+    )
+    toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info, llm=llm)
+    return toolkit.get_tools()
+
 
 langchain.verbose = True
 
 
 def create_index()-> VectorStoreIndexWrapper:
   loader = DirectoryLoader('./src/', glob="**/*.py")
-  return VectorstoreIndexCreator.from_loaders([loader])
+  return VectorstoreIndexCreator().from_loaders([loader])
 
 
 def chat(message:str, history: ChatMessageHistory, index:VectorStoreIndexWrapper) -> str:
   llm = ChatOpenAI(temperature=0)
   
-  return index.query(question=message, llm=llm)
+  tools = create_tools(index, llm)
+  
+  # memoryの初期化
+  memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True, chat_memory=history)
+  
+  agent_chain = initialize_agent(
+    tools,
+    llm,
+    agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
+    memory=memory
+  )
+  
+  return agent_chain.run(input=message)
 
